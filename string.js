03609970316dba99082e472010780c5f29e24f26
ihsanfikri12 <fikriabiyyu@gmail.com>
2019-09-21 16:29:55 +0700

    string and function

---
+++ string.js
@@ -0,0 +1,34 @@
+//------ String-------
+
+// EXAMPLE ES5
+
+// var name = "fikri";
+
+// console.log("my name is " + name);
+
+// // Example ES6 
+// var name6 = "Hani";
+
+// console.log(`my name is ${name6}`);
+
+//------ Var Let Const-------
+
+// Example ES5
+var jobs = "architect";
+
+for (var i = 0; i<1; i++) {
+    var jobs = "engineering";
+    console.log (jobs)
+}
+
+console.log(jobs);
+
+// Example ES6
+let jobs6 = "teacher";
+
+for(let i = 0; i<1; i++) {
+    let jobs6 = "police";
+    console.log(jobs6);
+}
+
+console.log(jobs6)
